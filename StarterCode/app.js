40ad0d424154591937feaea807ebc7d4de53a1b2
DarianR4 <79239254+DarianR4@users.noreply.github.com>
2021-07-23 03:24:28 -0400

    final one, i'm done

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,20 +1,4 @@
-// set the dimensions and margins of the graph
-// var margin = {top: 10, right: 30, bottom: 30, left: 60},
-//     width = 460 - margin.left - margin.right,
-//     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-// var svg = d3.select("#my_dataviz")
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
-// Load data from data.csv
-// d3.csv("./data.csv").then(function(healthcareData) {
-
-//     console.log(healthcareData)
 
 var width = parseInt(d3.select("#scatter").style("width"));
 
@@ -24,7 +8,7 @@ var margin = 20;
 
 var labelArea = 110;
 
-var tPadBottom = 40;
+var tPadBot = 40;
 
 var tPadLeft = 40;
 
@@ -58,7 +42,7 @@ function xTextRefresh() {
         "translate(" +
         ((width - labelArea) / 2 + labelArea) +
         ", " +
-        (height - margin - tPadBottom) +
+        (height - margin - tPadBot) +
         ")"
     );
 }
